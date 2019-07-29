a2399346771afc392e9d64e71217fb1a37be12ea
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-07-29 16:12:22 +0300

    task-02

--- 01/js/task-02.js
+++ 01/js/task-02.js
@@ -1,2 +1,9 @@
 const total = 100;
 const ordered = 50;
+let message;
+if (ordered <= total) {
+  message = `Заказ оформлен, с вами свяжется менеджер`;
+} else {
+  message = `На складе недостаточно товаров!`;
+}
+console.log(message);
