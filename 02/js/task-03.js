254f3ef139789fe7fadda31206c307e245aaf7a4
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-08-17 18:26:56 +0300

    task 2

---
+++ 02/js/task-03.js
@@ -0,0 +1,17 @@
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+);
+
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est", 40));
+
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est", 20));
