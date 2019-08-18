c24c549efb81c72d540347b93ab219044b81a4dc
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-08-18 22:55:52 +0300

    add task6

--- 02/js/task-06.js
+++ 02/js/task-06.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const checkForSpam = function(str) {
+  const string = str.toLowerCase();
+  return string.includes("spam") || string.includes("sale");
+};
+
+console.log(checkForSpam("Latest technology news"));
+
+console.log(checkForSpam("JavaScript weekly newsletter"));
+
+console.log(checkForSpam("Get best sale offers now!"));
+
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
