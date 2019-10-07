2e2d6e68677d1ad10f1b902b46809762c6ec79ba
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-10-07 20:01:09 +0300

    fix module4 task2

--- 04/js/task-02.js
+++ 04/js/task-02.js
@@ -1,22 +1,22 @@
 const inventory = {
-  items: ["Монорельса", "Фильтр"],
+  items: ['Монорельса', 'Фильтр'],
   add(itemName) {
     this.items.push(itemName);
   },
   remove(itemName) {
     this.items = this.items.filter(item => item !== itemName);
-  }
+  },
 };
 
 const invokeInventoryOperation = function(itemName, inventoryAction) {
   console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
-  inventoryAction(itemName);
+  inventoryAction.call(inventory);
 };
 
-invokeInventoryOperation("Аптечка", inventory.add.bind(inventory));
+invokeInventoryOperation('Аптечка', inventory.add);
 
 console.log(inventory.items);
 
-invokeInventoryOperation("Фильтр", inventory.remove.bind(inventory));
+invokeInventoryOperation('Фильтр', inventory.remove);
 
 console.log(inventory.items);
