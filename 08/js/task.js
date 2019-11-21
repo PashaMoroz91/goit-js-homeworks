6056f1432b2525ed6e110d08a33194ec7a588538
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-11-21 16:13:33 +0200

    fix

--- 08/js/task.js
+++ 08/js/task.js
@@ -1,7 +1,7 @@
 'use strict';
-​import galleryItems from './gallery-items.js';
-​const galleryList = document.querySelector('.gallery');
-​const createGalleryItems = galleryItems.reduce((acc, item) => {
+import galleryItems from './gallery-items.js';
+const galleryList = document.querySelector('.gallery');
+const createGalleryItems = galleryItems.reduce((acc, item) => {
   acc += ` <li class='gallery__item'> <a
      class="gallery__link"
      href="${item.original}"
@@ -12,21 +12,20 @@
      data-source="${item.original}"
      alt="${item.description}"
      />
-​
      <span class="gallery__icon">
      <i class="material-icons">zoom_out_map</i>
      </span>
      </a></li>`;
   return acc;
 }, '');
-​galleryList.innerHTML = createGalleryItems;
-​const lightbox = document.querySelector('.lightbox');
+galleryList.innerHTML = createGalleryItems;
+const lightbox = document.querySelector('.lightbox');
 const lightboxImage = document.querySelector('.lightbox___image');
 const lightboxContent = document.querySelector('.lightbox__content');
 const closeLightboxBtn = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
-​const openLightbox = function(event) {
+const openLightbox = function(event) {
   const galleryLink = Array.from(document.querySelectorAll('.gallery__link'));
   galleryLink.forEach(item => {
     if (
@@ -35,25 +34,23 @@ const closeLightboxBtn = document.querySelector(
       item.removeAttribute('href');
     }
   });
-​
   lightbox.classList.add('is-open');
   lightboxImage.setAttribute('src', event.target.getAttribute('data-source'));
   lightboxImage.setAttribute('alt', event.target.getAttribute('alt'));
 };
-​const closeLightbox = function(event) {
+const closeLightbox = function(event) {
   lightbox.classList.remove('is-open');
   lightboxImage.setAttribute('src', '');
   lightboxImage.setAttribute('alt', '');
 };
-​const closeLightboxOverlay = function(event) {
+const closeLightboxOverlay = function(event) {
   if (event.target === this) closeLightbox();
 };
-​const closeLightboxEsc = function(event) {
+const closeLightboxEsc = function(event) {
   if (event.keyCode === 27) closeLightbox();
 };
-​
 galleryList.addEventListener('click', openLightbox);
 closeLightboxBtn.addEventListener('click', closeLightbox);
 lightboxContent.addEventListener('click', closeLightboxOverlay);
 window.addEventListener('keydown', closeLightboxEsc);
-Collapse
+Collapse;
