e38d01b0d35ce3defd73955b1d99a185f7955c04
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-12-01 19:06:33 +0200

    fix task1

--- 11_promosification/js/task_1.js
+++ 11_promosification/js/task_1.js
@@ -1,18 +1,29 @@
-/* Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. 
-Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay. */
-
 'use strict';
 
+// const delay = ms => {
+//   return new Promise(resolved => {
+//     resolved(ms);
+//   });
+//   return Promise.resolve(ms);
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
 const delay = ms => {
-  return new Promise(resolved => {
-    resolved(ms);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
   });
-  // return Promise.resolve(ms);
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(1000).then(logger);
+delay(1500).then(logger);
+delay(2000).then(logger);
