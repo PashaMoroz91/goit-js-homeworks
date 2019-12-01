a10600ae83eca164eb9800bf36b88228447fab72
Pasha Moroz <pashamoroz@MacBook-Pro-Pasha.local>
2019-12-01 19:19:25 +0200

    fix task1.01

--- 11_promosification/js/task_1.js
+++ 11_promosification/js/task_1.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// const delay = ms => {
-//   return new Promise(resolved => {
-//     resolved(ms);
-//   });
-//   return Promise.resolve(ms);
-// };
-
-// const logger = time => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
-
 const delay = ms => {
   return new Promise(resolve => {
     setTimeout(() => {
